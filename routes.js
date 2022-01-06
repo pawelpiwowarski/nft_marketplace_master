3cd23803dee254f3a4630f55172fac8cd2b11323
pawelpiwowarski <pawelpiwowarski2000@gmail.com>
2022-01-06 20:45:49 +0100

    testing new filenames

--- routes.js
+++ routes.js
@@ -3,7 +3,6 @@ const routes = require('next-routes-extended')
 
 
 module.exports = routes()     
-.add('/asset/:instance_address/:index_of_the_nft', '/asset')
+.add('/asset/:instance_address/:index_of_the_nft', '/asset/0x8c97E997bC995De9010D90fCd69C9f2b529A4227/[index]')
 .add('/mint_nft', '/mint_nft')
-.add('/profile/:address', '/profile')
-
+.add('/profile/:address', '/profile/[index]')
