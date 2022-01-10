f80b298d666dbfea130d6b5d78aad9f05b8ecea5
pawelpiwowarski <pawelpiwowarski2000@gmail.com>
2022-01-10 13:38:06 +0100

    working on transaction handling errors v2

--- etherum_side/web3.js
+++ etherum_side/web3.js
@@ -4,20 +4,13 @@ let web3;
  
 if (typeof window !== "undefined" && typeof window.ethereum !== "undefined" ) {
   // We are in the browser and metamask is running.
-  if (window.ethereum.chainId == "0x4")
-  {
+  
   window.ethereum.request({ method: "eth_accounts" });
   web3 = new Web3(window.ethereum);
 
   
-  }
-  else {
+  
 
-    const provider = new Web3.providers.WebsocketProvider(
-      "wss://rinkeby.infura.io/ws/v3/6d6b6d3a7c164567a05a88d04c2a3c92"
-    );
-    web3 = new Web3(provider);
-  }
 } 
 
 else {
