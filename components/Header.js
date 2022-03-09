5fb94f81a74058088a46a510d956f1beb380ad1a
pawelpiwowarski <pawelpiwowarski2000@gmail.com>
2022-03-09 16:14:17 +0100

    menu bar bigger

--- components/Header.js
+++ components/Header.js
@@ -69,7 +69,7 @@ const Header = (props) => {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
-        <Menu size='large' inverted>
+        <Menu size='massive' inverted>
 
     
     <Menu.Item active={true} color='teal' />
